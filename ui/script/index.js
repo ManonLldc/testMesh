7c298bd858e2dd658460fed4121762af0ad03764
SilvinLeroy <leroy_silvin@yahoo.fr>
2025-09-23 17:41:28 +0200

    second try of ui upload

---
+++ ui/script/index.js
@@ -0,0 +1,55 @@
+var sendForm = document.querySelector('#chatform'),
+    textInput = document.querySelector('.chatbox'),
+    chatList = document.querySelector('.chatlist'),
+    userBubble = document.querySelectorAll('.userInput'),
+    overview = document.querySelector('.chatbot__overview'),
+    hasCorrectInput,
+    imgLoader = false,
+    animationCounter = 1,
+    animationBubbleDelay = 600,
+    input,
+    previousInput,
+    isReaction = false,
+    chatbotButton = document.querySelector(".submit-button")
+
+sendForm.onkeydown = function(e){
+  if(e.keyCode == 13){
+    e.preventDefault();
+
+    //No mix ups with upper and lowercases
+    var input = textInput.value.toLowerCase();
+
+    //Empty textarea fix
+    if(input.length > 0) {
+      createBubble(input)
+    }
+  }
+};
+
+sendForm.addEventListener('submit', function(e) {
+  //so form doesnt submit page (no page refresh)
+  e.preventDefault();
+
+  //No mix ups with upper and lowercases
+  var input = textInput.value.toLowerCase();
+
+  //Empty textarea fix
+  if(input.length > 0) {
+    createBubble(input)
+  }
+}) //end of eventlistener
+
+var createBubble = function(input) {
+  //create input bubble
+  var chatBubble = document.createElement('li');
+  chatBubble.classList.add('lichatlist');
+
+  //adds input of textarea to chatbubble list item
+  chatBubble.innerHTML = input;
+
+  //adds chatBubble to chatlist
+  chatList.appendChild(chatBubble)
+
+  checkInput(input);
+
+}
